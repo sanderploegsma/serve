c294bff8bbe75226ebf53e8e6747ae1e7eaecd57
Sander Ploegsma <sanderploegsma@gmail.com>
2023-02-15 20:38:02 +0100

    Use cobra for argument parsing

--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"os"
+
+	"github.com/spf13/cobra"
 )
 
 var (
 	version = "dev"
-	commit  = "none"
-	date    = "unknown"
 
-	versionOption = flag.Bool("v", false, "Prints the version")
+	port int
 )
 
-func main() {
-	flag.Parse()
+var rootCmd = &cobra.Command{
+	Use:     "serve path/to/serve",
+	Short:   "Serve helps you serve static content",
+	Long:    "A static content server for your command line, built with Go.",
+	Version: version,
+	Args:    cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Serving %s on port %d", args[0], port)
+	},
+}
 
-	if *versionOption {
-		fmt.Printf("Serve %s\nCommit: %s\nDate: %s", version, commit, date)
-		os.Exit(0)
-	}
+func init() {
+	rootCmd.Flags().IntVarP(&port, "port", "p", 0, "Port to serve on. A random free port will be used automatically if not provided")
+}
+
+func main() {
+	rootCmd.Execute()
 }
